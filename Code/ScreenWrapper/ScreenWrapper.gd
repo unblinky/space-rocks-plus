0aa46f2f48a2346d1b7e4e3e0352d6cccbcd6bbb
unblinky <47279470+unblinky@users.noreply.github.com>
2026-02-04 16:59:32 -0800

    Update ScreenWrapper.gd

--- Code/ScreenWrapper/ScreenWrapper.gd
+++ Code/ScreenWrapper/ScreenWrapper.gd
@@ -2,12 +2,13 @@ extends Node2D
 class_name ScreenWrapper
 
 func _process(_delta: float) -> void:
-	# Wrap.
+	# Check the sides.
 	if position.x > get_viewport().size.x:
 		position.x = 0
 	elif position.x < 0:
 		position.x = get_viewport().size.x
-		
+	
+	# Check top & bottom.
 	elif position.y > get_viewport().size.y:
 		position.y = 0
 	elif position.y < 0:
