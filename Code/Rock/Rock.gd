7a048cb4d826b8925b9f350d2bd2a8e85b509326
unblinky <47279470+unblinky@users.noreply.github.com>
2026-02-09 16:33:33 -0800

    New art assets.

--- Code/Rock/Rock.gd
+++ Code/Rock/Rock.gd
@@ -1,17 +1,17 @@
 extends ScreenWrapper
 class_name Rock
 
+@onready var sprites: AnimatedSprite2D = $Sprites
+
 var main: Main
 var velocity: Vector2 = Vector2.ZERO # px / sec.
 @export var speed: float = 150
 
 func _ready() -> void:
 	velocity = Vector2(randf_range(-speed, speed), randf_range(-speed, speed)) 
+	# HACK: Not dynamic.
+	sprites.frame = randi_range(0, 3) 
 
 func _process(delta: float) -> void:
 	super._process(delta)
 	position += velocity * delta
-
-	
-
-	
