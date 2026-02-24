f40bf539f5cec17398b40091d418d72f51566289
unblinky <47279470+unblinky@users.noreply.github.com>
2026-02-23 16:57:13 -0800

    some extra UFO cleanup.

--- Code/Ship/Ship.gd
+++ Code/Ship/Ship.gd
@@ -42,6 +42,7 @@ func _process(delta: float) -> void:
 
 	position += velocity * delta
 
+## Destroy the ship and free the resourses.
 func destroy():
 	player.ship = null
 	queue_free()
--- Code/UFO/UFO.gd
+++ Code/UFO/UFO.gd
@@ -1,14 +1,17 @@
 extends Area2D
 class_name UFO
 
+const BULLET = preload("res://Bullet/Bullet.tscn")
 @onready var up_down_timer: Timer = $UpDownTimer
+@onready var shoot_timer: Timer = $ShootTimer
 
-var speed: float = 300.0 # px / sec.
+var speed: float = 100.0 # px / sec.
 var velocity: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	# Signal hooks.
 	up_down_timer.timeout.connect(pick_up_down)
+	shoot_timer.timeout.connect(on_shoot)
 	
 	# Pick a screen edge.
 	if randi() % 2:
@@ -20,19 +23,31 @@ func _ready() -> void:
 	
 	position.y = randi_range(0, get_viewport().size.y)
 
+func on_shoot():
+	var bullet = BULLET.instantiate()
+	bullet.position = position
+	bullet.rotation_degrees = randf_range(0, 360)
+	get_parent().add_child(bullet)
 
 func _process(delta: float) -> void:
-	position += velocity * delta
+	position += velocity * delta # Update position.
 	
+	# Kill UFO if out of bounds.
 	if position.x > get_viewport().size.x:
 		destroy()
 	if position.x < 0:
 		destroy()
-	# TODO: Screenwrap vertically.
+	
+	# Screenwrap vertically.
+	if position.y < 0.0:
+		position.y = get_viewport().size.y
+	if position.y > get_viewport().size.y:
+		position.y = 0.0
 
 func destroy():
 	queue_free()
 
+## A function that will have the UFO pick up, down, or neither.
 func pick_up_down():
 	if velocity.y == 0.0:
 		if randi() % 2:
